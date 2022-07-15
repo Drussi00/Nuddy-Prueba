d0a25ebb9e1f407d31a758ba495bc3441a2cf87b
Daniel <daniel.russi@hotmail.com>
2022-07-14 21:42:51 -0500

    archivo incial nuddy(commit main)

---
+++ pages/product/[slug].js
@@ -0,0 +1,201 @@
+import {
+  Alert,
+  Box,
+  Button,
+  Card,
+  CircularProgress,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Rating,
+  Typography,
+  Divider,
+  ButtonGroup,
+} from "@mui/material";
+import Image from "next/image";
+import NextLink from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { useSnackbar } from "notistack";
+import Layout from "../../components/Layout";
+import classes from "../../utils/classes";
+import client from "../../utils/client";
+import { urlFor, urlForThumbnail } from "../../utils/image";
+import { Store } from "../../utils/Store";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function ProductScreen(props) {
+  const router = useRouter();
+  const { slug } = props;
+  const {
+    state: { cart },
+    dispatch,
+  } = useContext(Store);
+  const { enqueueSnackbar } = useSnackbar();
+  const [state, setState] = useState({
+    product: null,
+    loading: true,
+    error: "",
+  });
+  const { product, loading, error } = state;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const product = await client.fetch(
+          `
+            *[_type == "product" && slug.current == $slug][0]`,
+          { slug }
+        );
+        setState({ ...state, product, loading: false });
+      } catch (err) {
+        setState({ ...state, error: err.message, loading: false });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      enqueueSnackbar("Sorry. Product is out of stock", { variant: "error" });
+      return;
+    }
+
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: {
+        _key: product._id,
+        name: product.name,
+        countInStock: product.countInStock,
+        slug: product.slug.current,
+        price: product.price,
+        image: urlForThumbnail(product.image),
+        quantity,
+      },
+    });
+    enqueueSnackbar(`${product.name} added to the cart`, {
+      variant: "success",
+    });
+    router.push("/cart");
+  };
+  return (
+    <Layout title={product?.title}>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert variant="error">{error}</Alert>
+      ) : (
+        <Box>
+          <Grid container spacing={6}>
+            <Grid item md={6} xs={12}>
+              <Image
+                src={urlFor(product.image)}
+                alt={product.name}
+                layout="responsive"
+                width={640}
+                height={640}
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <List>
+                <ListItem>
+                  <Typography sx={classes.title}>{product.name}</Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={classes.bold}>${product.price}</Typography>
+                </ListItem>
+                <Divider sx={classes.line} />
+                <ListItem>
+                  <Typography>Tallas</Typography>
+                </ListItem>
+                <listItem>
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <Button
+                        sx={classes.but}
+                        width="small"
+                        variant="contained"
+                      >
+                        S
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button sx={classes.but} size="small" variant="contained">
+                        M
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button sx={classes.but} size="small" variant="contained">
+                        L
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </listItem>
+                <ListItem>
+                  <Typography>Cantidad</Typography>
+                </ListItem>
+                <ButtonGroup
+                  sx={classes.buttonGroup}
+                  variant="outlined"
+                  aria-label="outlined button group"
+                >
+                  <Button color="primary" sx={classes.buttonGroup}>
+                    -
+                  </Button>
+                  <Button sx={classes.buttonQ}>0</Button>
+                  <Button sx={classes.buttonGroup}>+</Button>
+                </ButtonGroup>
+                <ListItem>
+                  <Button
+                    sx={classes.blackline}
+                    onClick={addToCartHandler}
+                    fullWidth
+                    variant=""
+                  >
+                    Agregar al carrito
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <Button
+                    onClick={addToCartHandler}
+                    fullWidth
+                    variant="contained"
+                    sx={classes.radius}
+                  >
+                    Comprar ahora
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <Grid container spacing={0}>
+                    <Grid item md={6}>
+                      <Typography fontSize="0.6rem">
+                        {product.description}
+                      </Typography>
+                      <Typography fontSize="0.6rem">
+                        {product.materiales}
+                      </Typography>
+                      <Typography fontSize="0.6rem">
+                        {product.cantidadmateriales}
+                      </Typography>
+                      <Typography fontSize="0.6rem">{product.envio}</Typography>
+                    </Grid>
+                    <ListItem></ListItem>
+                  </Grid>
+                </ListItem>
+              </List>
+            </Grid>
+          </Grid>
+        </Box>
+      )}
+    </Layout>
+  );
+}
+
+export function getServerSideProps(context) {
+  return {
+    props: { slug: context.params.slug },
+  };
+}
---
+++ utils/classes.js
@@ -0,0 +1,88 @@
+import { height } from "@mui/system";
+
+const classes = {
+  section: {
+    marginTop: 1,
+    marginBottom: 1,
+  },
+  smallText: {
+    fontSize: "13px",
+  },
+  main: {
+    marginTop: 2,
+    minHeight: "80vh",
+  },
+  footer: {
+    marginTop: 1,
+    textAlign: "center",
+  },
+  appbar: {
+    backgroundColor: "#000000",
+    "& a": {
+      color: "#ffffff",
+      marginLeft: 1,
+    },
+    borderRadius: "0 0 15px 15px",
+  },
+  toolbar: {
+    justifyContent: "space-between",
+  },
+  brand: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+  },
+  navbarButton: {
+    color: "#ffffff",
+    textTransform: "initial",
+  },
+  fullWidth: {
+    width: "100%",
+  },
+  sort: {
+    marginRight: 1,
+  },
+  visible: {
+    display: "initial",
+  },
+  hidden: {
+    display: "none",
+  },
+  // search
+
+  searchForm: {
+    border: "1px solid #ffffff",
+    backgroundColor: "#ffffff",
+    borderRadius: 1,
+  },
+  searchInput: {
+    paddingLeft: 1,
+    color: "#000000",
+    "& ::placeholder": {
+      color: "#606060",
+    },
+  },
+  searchButton: {
+    backgroundColor: "#f8c040",
+    padding: 1,
+    borderRadius: "0 5px 5px 0",
+    "& span": {
+      color: "#000000",
+    },
+  },
+  title: { fontWeight: "bold", fontSize: "3rem" },
+  bold: { fontWeight: "bold", fontSize: "2.2rem" },
+  line: { background: "black", height: "2px" },
+  but: { fontSize: "12px", border: "1px solid black ", borderWidth: ".1" },
+  blackline: {
+    borderBottomStyle: "solid",
+    fontWeight: "bold",
+    textDecoration: "underline ",
+    borderWidth: "1",
+    textDecorationThickness: "1.5px",
+  },
+  radius: { borderRadius: "100px" },
+  buttonQ: { width: "80px", borderWidth: "1px", borderColor: "black" },
+  buttonGroup: { borderWidth: "1px", borderColor: "black" },
+};
+
+export default classes;
