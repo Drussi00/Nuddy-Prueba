5d61c63854d15c02b0e6274a4fd2db981bd5f89d
Daniel <daniel.russi@hotmail.com>
2022-08-03 00:58:55 -0500

    final con guille(commit main)

--- pages/api/orders/MercadoPago/index.js
+++ pages/api/orders/MercadoPago/index.js
@@ -2,31 +2,31 @@ import mercadopago from "../../../../utils/mercadoPago";
 import nc from "next-connect";
 const handler = nc();
 
-handler.post(async(req, res) => {
-    let preference = {
-        items: [
-            {
-                title: req.body.orderItems[0].name,
-                unit_price: req.body.orderItems[0].quantity,
-                quantity: 1,
-                description: req.body.orderItems[0].size
-            }
-        ]
-    };
-    
-    try {
-      const response = await mercadopago.mercadopago.preferences.create(preference);
-      console.log(response)
-      res.status(201).json({
-        global: response.body.id
-        });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({error:"error en la peticion"})
-    }
+handler.post(async (req, res) => {
+  let preference = {
+    items: [
+      {
+        title: req.body.orderItems[0].name,
+        unit_price: req.body.orderItems[0].price,
+        quantity: req.body.orderItems[0].quantity,
 
+        description: req.body.orderItems[0].size,
+      },
+    ],
+  };
+  console.log(req.body.orderItems);
+  try {
+    const response = await mercadopago.mercadopago.preferences.create(
+      preference
+    );
 
-  });
+    res.status(201).json({
+      global: response.body.id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "error en la peticion" });
+  }
+});
 
-  
 export default handler;
