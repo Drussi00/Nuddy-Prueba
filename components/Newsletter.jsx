c5ee103401501770bf59aec80cb4b6a4243be278
Daniel <daniel.russi@hotmail.com>
2022-09-07 22:56:24 -0500

    commit main

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -6,14 +6,21 @@ import {
   Typography,
   useMediaQuery,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Checkbox from "@mui/material/Checkbox";
 import axios from "axios";
 
 const Newsletter = () => {
   const isDesktop = useMediaQuery("(min-width:600px)");
-  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   const [email, setEmail] = useState("");
+  const [checked, setchecked] = useState(true);
+
+  useEffect(() => {
+    const emailInput = document.getElementById("newsletter");
+
+    emailInput.disabled = checked;
+    return () => {};
+  }, [checked]);
 
   const subscribe = async () => {
     try {
@@ -51,6 +58,7 @@ const Newsletter = () => {
             style={{ width: isDesktop ? "40%" : "100%" }}
           >
             <input
+              id="newsletter"
               type="text"
               className="form-control"
               placeholder="Agrega tu correo electronico "
@@ -60,7 +68,15 @@ const Newsletter = () => {
             <span className="input-group-btn">
               <Button
                 className="btn "
-                sx={{ backgroundColor: "black", color: "white" }}
+                sx={{
+                  backgroundColor: "black",
+                  color: "white",
+                  "&:hover": {
+                    backgroundColor: "black",
+                    color: "white",
+                    transform: "scale(1.1, 1.1)",
+                  },
+                }}
                 type="button"
                 onClick={subscribe}
               >
@@ -77,8 +93,9 @@ const Newsletter = () => {
           }}
         >
           <Checkbox
-            {...label}
-            defaultChecked
+            onClick={() => {
+              setchecked(!checked);
+            }}
             sx={{ "& .MuiSvgIcon-root": { fontSize: 28 } }}
           />
           <div style={{ height: "40px" }}>
