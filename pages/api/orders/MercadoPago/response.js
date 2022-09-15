d36d13b222231fc282b6c6f399fee868bbc645a1
Daniel <daniel.russi@hotmail.com>
2022-09-15 01:44:07 -0500

    f

--- pages/api/orders/MercadoPago/response.js
+++ pages/api/orders/MercadoPago/response.js
@@ -2,7 +2,10 @@ import nc from "next-connect";
 
 const handler = nc();
 
+import nc from "next-connect";
+
 handler.post(async (req, res) => {
   console.log(req.body);
   res.status(200).send("Ok");
 });
+export default handler;
