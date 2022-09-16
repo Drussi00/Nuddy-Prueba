c6d7f22663073958b017d66484991d5ab2afeb8e
Daniel <daniel.russi@hotmail.com>
2022-09-16 11:04:13 -0500

    cors

--- pages/api/orders/MercadoPago/response.js
+++ pages/api/orders/MercadoPago/response.js
@@ -1,4 +1,12 @@
-export default function handler(req, res) {
+import NextCors from "nextjs-cors";
+
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   console.log(req.body);
   res.status(200).json({ Ok: true });
 }
