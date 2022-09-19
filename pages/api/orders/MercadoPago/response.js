b05f19026e54c005a6204c77083034649ea7bdf5
Daniel <daniel.russi@hotmail.com>
2022-09-19 11:13:38 -0500

    m

--- pages/api/orders/MercadoPago/response.js
+++ pages/api/orders/MercadoPago/response.js
@@ -1,5 +1,6 @@
 import NextCors from "nextjs-cors";
-
+import axios from "axios";
+i;
 export default async function handler(req, res) {
   await NextCors(req, res, {
     // Options
@@ -7,8 +8,13 @@ export default async function handler(req, res) {
     origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
+
   try {
-    console.log(req.query["data.id"]);
+    const id = req.query["data.id"];
+    const resMercadoPago = await axios.get(
+      `https://api.mercadopago.com/v1/payments/${id}?access_token=${process.env.MERCADO_PAGO_ACCESS_TOKEN}`
+    );
+    console.log(resMercadoPago);
   } catch (error) {
     console.log(req.query["data.id"]);
 
